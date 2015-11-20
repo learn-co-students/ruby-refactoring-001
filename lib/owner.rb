14f7f81696c52555ebec01e46bf26a22ebb73973
maiya777 <maiya777@list.ru>
2015-11-20 13:16:23 +0300

    Done

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,22 @@
 class Owner
-  # code your solution here!
+ 
+  attr_accessor :name, :dog
+
+  def walk
+    # REFACTOR!
+    prepare_items
+    self.dog.walking = true
+  end
+
+  def vet_checkup
+    # REFACTOR!
+    prepare_items
+    self.dog.vet_checkup = true
+  end
+
+  def prepare_items
+    self.dog.leash = true
+    self.dog.plastic_bag = true
+  end
+
 end
