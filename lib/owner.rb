645eb55fc9b020d688763b6b5ed76224b32ad2f8
fhernandez173 <f.hernandez173@gmail.com>
2016-07-13 13:36:08 -0400

    refactor:smelly code

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
+require 'pry'
+
 class Owner
-  # code your solution here!
+
+  attr_accessor :name, :dog
+
+  def initialize(name)
+    @name = name
+    # @dog = Dog.new
+  end
+
+  def prepare_items
+    self.dog.leash = true
+    self.dog.plastic_bag = true
+  end
+
+  def walk#(owner)
+    # REFACTOR!
+    prepare_items
+    self.dog.walking = true
+  end
+
+  def vet_checkup
+    prepare_items
+    self.dog.vet_checkup = true
+  end
+
 end
